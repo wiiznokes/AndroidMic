54337d2fbed06da6285d499924b18f17c609f295
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2025-12-12 19:34:25 +0100

    Active mode + clean impl

--- Android/app/src/main/java/io/github/teamclouday/AndroidMic/ui/ConnectTileService.kt
+++ Android/app/src/main/java/io/github/teamclouday/AndroidMic/ui/ConnectTileService.kt
@@ -1,7 +1,6 @@
 package io.github.teamclouday.AndroidMic.ui
 
 import android.content.ComponentName
-import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.*
@@ -9,51 +8,83 @@ import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import android.util.Log
 import androidx.annotation.RequiresApi
+import io.github.teamclouday.AndroidMic.AppPreferences
+import io.github.teamclouday.AndroidMic.Dialogs
 import io.github.teamclouday.AndroidMic.domain.service.*
+import io.github.teamclouday.AndroidMic.utils.Either
+import io.github.teamclouday.AndroidMic.utils.ignore
+import kotlinx.coroutines.runBlocking
 
 @RequiresApi(Build.VERSION_CODES.N)
 class ConnectTileService : TileService() {
 
     private val TAG = "ConnectTileService"
 
-    private var serviceMessenger: Messenger? = null
-    private var isBound = false
-    private var isStreaming = false
+    private val prefs = AppPreferences(this)
 
-    private lateinit var responseMessenger: Messenger
+    private var service: Messenger? = null
+    private var isBound = false
     private lateinit var handlerThread: HandlerThread
-    private lateinit var serviceLooper: Looper
+    private lateinit var messenger: Messenger
+    private lateinit var messengerLooper: Looper
+    private var isStreamStarted = false
+    private var isClickable = true
+
 
     private inner class ResponseHandler(looper: Looper) : Handler(looper) {
         override fun handleMessage(msg: Message) {
             val response = ResponseData.fromMessage(msg)
-            isStreaming = response.state == ServiceState.Connected
+
+            when (response.kind) {
+                Response.Standard -> {
+                    response.state?.let {
+                        isClickable = true
+                        isStreamStarted = it == ServiceState.Connected
+                    }
+                }
+            }
             updateTile()
         }
     }
 
     override fun onCreate() {
         super.onCreate()
-        handlerThread = HandlerThread("TileServiceResponse")
+        handlerThread =
+            HandlerThread("TileServiceResponseHandler", Process.THREAD_PRIORITY_BACKGROUND)
         handlerThread.start()
-        serviceLooper = handlerThread.looper
-        responseMessenger = Messenger(ResponseHandler(serviceLooper))
+        messengerLooper = handlerThread.looper
+        messenger = Messenger(ResponseHandler(messengerLooper))
+
+        val intent = Intent(this, ForegroundService::class.java).apply {
+            action = BIND_SERVICE_ACTION
+        }
+        startService(intent)
+        bindService(intent, connection, BIND_AUTO_CREATE)
     }
 
     private val connection = object : ServiceConnection {
         override fun onServiceConnected(className: ComponentName, service: IBinder) {
             Log.d(TAG, "onServiceConnected")
-            serviceMessenger = Messenger(service)
+            this@ConnectTileService.service = Messenger(service)
             isBound = true
-            // Request status update
-            sendCommand(Command.GetStatus)
+
+            requestListeningState(
+                this@ConnectTileService,
+                ComponentName(this@ConnectTileService, ConnectTileService::class.java)
+            )
+
+            val message = Message.obtain()
+            message.what = Command.GetStatus.ordinal
+            message.replyTo = messenger
+            this@ConnectTileService.service?.send(message)
         }
 
         override fun onServiceDisconnected(arg0: ComponentName) {
             Log.d(TAG, "onServiceDisconnected")
-            serviceMessenger = null
+            service = null
             isBound = false
-            isStreaming = false
+            isStreamStarted = false
+            isClickable = false
             updateTile()
         }
     }
@@ -61,61 +92,65 @@ class ConnectTileService : TileService() {
     override fun onStartListening() {
         super.onStartListening()
         Log.d(TAG, "onStartListening")
-        Intent(this, ForegroundService::class.java).also { intent ->
-            bindService(intent, connection, Context.BIND_AUTO_CREATE)
-        }
     }
 
     override fun onStopListening() {
         super.onStopListening()
         Log.d(TAG, "onStopListening")
+    }
+
+    override fun onClick() {
+        super.onClick()
+        Log.d(TAG, "onClick")
+
+        if (!isBound) {
+            Log.d(TAG, "service not bound, can't send command")
+            return
+        }
+
+        if (service == null) {
+            Log.d(TAG, "service is null, can't send command")
+            return
+        }
+
+        val reply = if (isStreamStarted) {
+            CommandData(Command.StopStream)
+        } else {
+            val res = runBlocking {
+                CommandData.fromPref(prefs, Command.StartStream)
+            }
+            when (res) {
+                is Either.Left<CommandData> -> {
+                    res.value
+                }
+
+                is Either.Right<Dialogs> -> {
+                    Log.d(TAG, "missing parameter to start stream")
+                    return
+                }
+            }
+        }.toCommandMsg()
+
+        isClickable = false
+        reply.replyTo = messenger
+        service?.send(reply)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        messengerLooper.quitSafely()
+        ignore { handlerThread.join(WAIT_PERIOD) }
         if (isBound) {
             unbindService(connection)
             isBound = false
         }
     }
 
-    override fun onClick() {
-        super.onClick()
-        Log.d(TAG, "onClick")
-        if (isStreaming) {
-            sendCommand(Command.StopStream)
-        } else {
-            val intent = Intent(this, MainActivity::class.java).apply {
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            }
-            startActivityAndCollapse(intent)
-        }
-    }
-    
-    override fun onDestroy() {
-        super.onDestroy()
-        serviceLooper.quitSafely()
-        try {
-            handlerThread.join(500)
-        } catch (e: InterruptedException) {
-            // ignore
-        }
-    }
-
-    private fun sendCommand(command: Command, commandData: CommandData? = null) {
-        if (!isBound) return
-        val msg = Message.obtain(null, command.ordinal)
-        msg.replyTo = responseMessenger
-        if (commandData != null) {
-            msg.data = commandData.toBundle()
-        }
-        try {
-            serviceMessenger?.send(msg)
-        } catch (e: RemoteException) {
-            Log.e(TAG, "Failed to send message to service", e)
-        }
-    }
-
     private fun updateTile() {
         val tile = qsTile
         if (tile != null) {
-            tile.state = if (isStreaming) Tile.STATE_ACTIVE else Tile.STATE_INACTIVE
+            tile.state = if (isStreamStarted) Tile.STATE_ACTIVE else Tile.STATE_INACTIVE
             tile.updateTile()
         }
     }
